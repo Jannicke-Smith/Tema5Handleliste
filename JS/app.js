947047bc604d6c72c51b404d83bbdb53514094f8
Jannicke-Smith <114487881+Jannicke-Smith@users.noreply.github.com>
2023-02-07 11:18:17 +0100

    liste

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,38 @@
+console.log ('flex');
+
+//input text element-
+
+//button element
+
+// get list
+
+// event listener som lytter på klikk på knapp
+//lese vderdi på input text og legge til im lista
+
+// eventlistener som lytter på tastarturknapp enter
+//lese vderdi på input text og legge til im lista
+
+
+const inputElement = document.getElementById('product');
+
+const buttonAddElement = document.getElementById('add-button');
+
+const ulShoppingListElement = document.getElementById('shopping-list');
+
+buttonAddElement.addEventListener('click', renderList);
+window.addEventListener('keyup', (event) => {
+    if(event.code === 'Enter') {
+        renderList();
+    }
+});
+
+function renderList() {
+    const liElement = document.createElement('li');
+    liElement.textContent = inputElement.value;
+    inputElement.value
+    ulShoppingListElement.appendChild(liElement);
+}
+
 
 function initList () {
     const bodyElement = document.querySelector('body');
